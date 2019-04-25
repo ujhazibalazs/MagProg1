760b7821871d7b5300e30b28fdf67e67ab0267ed
Ujházi Balázs <43323332+ujhazibalazs@users.noreply.github.com>
2019-04-25 09:21:57 +0200

    Create vegtelenciklus.c

---
+++ vegtelenciklus.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+            int main()
+            {
+                    for(;;)
+                    {
+                    	printf("fut\n");
+                       
+                    }
+                    return 0;
+            }
